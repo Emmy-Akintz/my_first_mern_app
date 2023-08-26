5931dcd98143c46698164b1d3e86e7f2248a86b3
Ayomide Akintan <emzyakints2005@gmail.com>
2023-08-26 15:15:13 +0100

    Fetching now works!
    My database now displays on my frontend

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
+// import axios from 'axios'
 
 function Home() {
   const [workouts, setWorkouts] = useState(null)
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch('/api/workout')
+      const response = await fetch('http://localhost:4000/api/workouts/')
       const json = await response.json()
 
       if (response.ok) {
@@ -14,15 +15,19 @@ function Home() {
     }
 
     fetchWorkouts()
+    
+    // axios.get('http://localhost:4000/api/workouts/')
+    // .then(result => setWorkouts(result.data))
+    // .catch(err => console.log(err))
   }, [])
 
   return (
     <div className='home'>
-        <div className="workouts">
-          {workouts && workouts.map((workout) => (
-            <p key={workout._id}>{workout.title }</p>
-          ))}
-        </div>
+      <div className="workouts">
+        {workouts && workouts.map((workout) => (
+          <p key={workout._id}>{workout.title}</p>
+        ))}
+      </div>
     </div>
   )
 }
