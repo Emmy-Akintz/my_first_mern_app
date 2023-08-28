cb4ada10011cea52a6943a9e26e04375f2e48775
Ayomide Akintan <emzyakints2005@gmail.com>
2023-08-28 16:01:02 +0100

    Update

---
+++ client/src/components/WorkoutForm.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+function WorkoutForm() {
+    const [title, setTitle] = useState('')
+    const [load, setLoad] = useState('')
+    const [reps, setReps] = useState('')
+
+  return (
+    <div>WorkoutForm</div>
+  )
+}
+
+export default WorkoutForm
