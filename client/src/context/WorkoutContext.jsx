f955c7929f0950fea2ad9edaaaf1426cc2cc515c
Ayomide Akintan <emzyakints2005@gmail.com>
2023-09-03 17:10:49 +0100

    Update

--- client/src/context/WorkoutContext.jsx
+++ client/src/context/WorkoutContext.jsx
@@ -1,20 +1,25 @@
-import React, { createContext } from 'react'
+import React, { createContext, useReducer } from 'react'
 
 export const WorkoutsContext = createContext()
 
 export const WorkoutsContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(workoutsReducer, {
+    workouts: null
+  })
 
-    return (
-        <WorkoutsContext.Provider>
-            
-        </WorkoutsContext.Provider>
-    )
-}
+  dispatch({type: 'SET_WORKOUT', payload: [{}, {}]})
 
-function WorkoutContext() {
   return (
-    <div>WorkoutContext</div>
+    <WorkoutsContext.Provider value={ }>
+      {children}
+    </WorkoutsContext.Provider>
   )
 }
 
-export default WorkoutContext
+// function WorkoutContext() {
+//   return (
+//     <div>WorkoutContext</div>
+//   )
+// }
+
+// export default WorkoutContext
