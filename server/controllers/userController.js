cc6824a9d7607477f839ba650fba005aee312150
Ayomide Akintan <emzyakints2005@gmail.com>
2023-11-29 17:24:40 +0100

    tut7 completed

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -7,7 +7,22 @@ const createToken = (_id) => {
 
 // login user jwt.io
 const loginUser = async (req, res) => {
-    res.json({ mssg: 'login user' })
+    const {email, password} = req.body
+
+    try {
+        const user = await User.login(email, password)
+
+        // create a token
+        const token = createToken(user._id)
+
+        res.status(200).json({
+            email,
+            // user,
+            token
+        })
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
 }
 
 // signup user jwt.io
