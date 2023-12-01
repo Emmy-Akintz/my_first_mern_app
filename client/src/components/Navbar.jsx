1fe563510aab6f48bb357640901b02510a9ddcfb
Ayomide Akintan <emzyakints2005@gmail.com>
2023-12-01 17:30:41 +0100

    tut11 done

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,20 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
 
 function Navbar() {
+  const { logout } = useLogout()
+
+  const handleClick = () => {
+    logout()
+  }
+
   return (
     <header>
-        <div className="container">
-            <Link to='/'>
-                <h1>Workout Buddy</h1>
-            </Link>
-            <nav>
-              <div>
-                <Link to='/login'>Login</Link>
-                <Link to='/signup'>Signup</Link>
-              </div>
-            </nav>
-        </div>
+      <div className="container">
+        <Link to='/'>
+          <h1>Workout Buddy</h1>
+        </Link>
+        <nav>
+          <div>
+            <button onClick={handleClick}>Log out</button>
+          </div>
+          <div>
+            <Link to='/login'>Login</Link>
+            <Link to='/signup'>Signup</Link>
+          </div>
+        </nav>
+      </div>
     </header>
   )
 }
