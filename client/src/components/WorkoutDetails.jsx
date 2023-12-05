8135f0a63a7102576c63b306152f82414c60ef32
Ayomide Akintan <emzyakints2005@gmail.com>
2023-12-05 15:02:05 +0100

    tut15 done

--- client/src/components/WorkoutDetails.jsx
+++ client/src/components/WorkoutDetails.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+import { useAuthContext } from '../hooks/useAuthContext'
 import '../index.css'
 
 // date-fns
@@ -7,25 +8,32 @@ import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 
 function WorkoutDetails({ workout }) {
   const { dispatch } = useWorkoutsContext()
+  const { user } = useAuthContext()
 
   const handleClick = async () => {
+    if (!user) {
+      return
+    }
     const response = await fetch('http://localhost:4000/api/workouts/' + workout._id, {
-      method: 'DELETE'
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Bearer ${user.token}`
+      }
     })
     const json = await response.json()
 
     if (response.ok) {
-      dispatch({type: 'DELETE_WORKOUT', payload: json})
+      dispatch({ type: 'DELETE_WORKOUT', payload: json })
     }
   }
 
   return (
     <div className='workout-details'>
-        <h4>{workout.title}</h4>
-        <p><strong>Load (kg): </strong>{workout.load}</p>
-        <p><strong>Reps: </strong>{workout.reps}</p>
-        <p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}</p>
-        <span className='material-symbols-outlined' onClick={handleClick}>delete</span>
+      <h4>{workout.title}</h4>
+      <p><strong>Load (kg): </strong>{workout.load}</p>
+      <p><strong>Reps: </strong>{workout.reps}</p>
+      <p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}</p>
+      <span className='material-symbols-outlined' onClick={handleClick}>delete</span>
     </div>
   )
 }
--- client/src/components/WorkoutForm.jsx
+++ client/src/components/WorkoutForm.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+import { useAuthContext } from '../hooks/useAuthContext'
 import '../index.css'
 
 function WorkoutForm() {
   const { dispatch } = useWorkoutsContext()
+  const { user } = useAuthContext()
 
   const [title, setTitle] = useState('')
   const [load, setLoad] = useState('')
@@ -14,29 +16,35 @@ function WorkoutForm() {
   const handleSubmit = async (e) => {
     e.preventDefault()
 
-    const workout = {title, load, reps}
+    if (!user) {
+      setError('You must be logged in')
+      return
+    }
+
+    const workout = { title, load, reps }
 
     const response = await fetch('http://localhost:4000/api/workouts', {
       method: 'POST',
       body: JSON.stringify(workout),
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${user.token}`
       }
     })
     const json = await response.json()
 
-    if(!response.ok) {
+    if (!response.ok) {
       setError(json.error)
       setEmptyFields(json.emptyFields)
     }
-    if(response.ok) {
+    if (response.ok) {
       setTitle('')
       setLoad('')
       setReps('')
       setError(null)
       setEmptyFields([])
       console.log('new workout added', json)
-      dispatch({type: 'CREATE_WORKOUT', payload: json})
+      dispatch({ type: 'CREATE_WORKOUT', payload: json })
     }
   }
 
